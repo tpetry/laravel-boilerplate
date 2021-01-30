d1567bca4f2b6fc03cada7449042dfe7eeca0129
Tobias Petry <tpetry@users.noreply.github.com>
2021-01-30 11:30:03 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Frontend/User/ProfileController.php
+++ app/Http/Controllers/Frontend/User/ProfileController.php
@@ -9,17 +9,19 @@ use App\Http\Requests\Frontend\User\UpdateProfileRequest;
 /**
  * Class ProfileController.
  */
-class ProfileController extends Controller {
+class ProfileController extends Controller
+{
     /**
      * @param  UpdateProfileRequest  $request
      * @param  UserService  $userService
      *
      * @return mixed
      */
-    public function update(UpdateProfileRequest $request, UserService $userService) {
+    public function update(UpdateProfileRequest $request, UserService $userService)
+    {
         $userService->updateProfile($request->user(), $request->validated());
 
-        if(session()->has('resent')) {
+        if (session()->has('resent')) {
             return redirect()
                 ->route('frontend.auth.verification.notice')
                 ->withFlashInfo(__('You must confirm your new e-mail address before you can go any further.'));
